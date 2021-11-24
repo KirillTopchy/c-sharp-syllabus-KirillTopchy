7b479e712fb9ad1116fc784306f6c27554e97da0
Kirils Topčijs <kirilltopchy@gmail.com>
2021-11-24 19:29:46 +0200

    Exercise 4 completed

---
+++ csharp-basics/exercises/Arithmetic/Exercise 4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 1;
+            for (var i = 1; i < 10; i++)
+            {
+                result += result * i;
+            }
+            Console.WriteLine("10! = {0}", result);
+        }
+    }
+}
