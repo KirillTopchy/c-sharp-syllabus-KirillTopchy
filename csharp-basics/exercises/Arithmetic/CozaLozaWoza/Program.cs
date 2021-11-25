12a3404bdcdc5152f91c95a3151abb603233bcb4
Kirils Topčijs <kirilltopchy@gmail.com>
2021-11-25 20:05:03 +0200

    Exercise 6 updated.

--- csharp-basics/exercises/Arithmetic/CozaLozaWoza/Program.cs
+++ csharp-basics/exercises/Arithmetic/CozaLozaWoza/Program.cs
@@ -8,41 +8,38 @@ namespace CozaLozaWoza
         {
             for (var i = 1; i <= 110; i++)
             {
-                switch (i % 3)
+                if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0)
                 {
-                    case 0 when i % 5 == 0 && i % 7 == 0:
-                        Console.Write("CozaLozaWoza ");
-                        break;
-                    case 0 when i % 5 == 0:
-                        Console.Write("CozaLoza ");
-                        break;
-                    case 0 when i % 7 == 0:
-                        Console.Write("CozaWoza ");
-                        break;
-                    default:
-                    {
-                        if (i % 5 == 0 && i % 7 == 0)
-                        {
-                            Console.Write("LozaWoza ");
-                        }
-                        else if (i % 3 == 0)
-                        {
-                            Console.Write("Coza ");
-                        }
-                        else if (i % 5 == 0)
-                        {
-                            Console.Write("Loza ");
-                        }
-                        else if (i % 7 == 0)
-                        {
-                            Console.Write("Woza ");
-                        }
-                        else
-                        {
-                            Console.Write(i+" ");
-                        }
-                        break;
-                    }
+                    Console.Write("CozaLozaWoza ");
+                }
+                else if(i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.Write("CozaLoza ");
+                }
+                else if (i % 3 == 0 && i % 7 == 0)
+                {
+                    Console.Write("CozaWoza ");
+                }
+                else if (i % 5 == 0 && i % 7 == 0)
+                {
+                    Console.Write("LozaWoza ");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.Write("Coza ");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.Write("Loza ");
+                }
+
+                else if (i % 7 == 0)
+                {
+                    Console.Write("Woza ");
+                }
+                else
+                {
+                    Console.Write(i + " ");
                 }
             }
         }
