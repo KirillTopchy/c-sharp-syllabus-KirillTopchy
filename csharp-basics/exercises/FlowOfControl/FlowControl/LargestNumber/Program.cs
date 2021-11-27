bfb7856711268a34676d195c16f12a0f6643c7c8
Kirils Topčijs <kirilltopchy@gmail.com>
2021-11-27 08:33:28 +0200

    Exercise 1 completed.

--- csharp-basics/exercises/FlowOfControl/FlowControl/LargestNumber/Program.cs
+++ csharp-basics/exercises/FlowOfControl/FlowControl/LargestNumber/Program.cs
@@ -4,24 +4,41 @@ namespace LargestNumber
 {
     class Program
     {
-        //TODO: Write a C# program to to find the largest of three numbers.
         static void Main(string[] args)
         {
             Console.WriteLine("Input the 1st number: ");
-            var input1 = Console.ReadLine();
+            var input1 = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Input the 2nd number: ");
-            var input2 = Console.ReadLine();
+            var input2 = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Input the 3rd number: ");
-            var input3 = Console.ReadLine();
-        
-            /*
-            todo - expected output:
-            Input the 1st number: 25
-            Input the 2nd number: 78
-            Input the 3rd number: 87
-             */
+            var input3 = Convert.ToInt32(Console.ReadLine());
+
+            int[] inputArray = {input1, input2, input3};
+
+            Array.Sort(inputArray);
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (inputArray[i] % 1 == 0)
+                {
+                    Console.WriteLine($"Input the {i+1}" + DisplaySuffix(i+1) +
+                                      $" number: {inputArray[i]}");
+                }
+            }
+            Console.ReadKey();
+        }
+        public static string DisplaySuffix(int num)
+        {
+            var number = num.ToString();
+            if (number.EndsWith("11")) return "th";
+            if (number.EndsWith("12")) return "th";
+            if (number.EndsWith("13")) return "th";
+            if (number.EndsWith("1")) return "st";
+            if (number.EndsWith("2")) return "nd";
+            if (number.EndsWith("3")) return "rd";
+            return "th";
         }
     }
 }
