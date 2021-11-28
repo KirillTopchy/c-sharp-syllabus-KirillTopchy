b5559a217b170c2bd0cdc813f67c1df89c42b616
Kirils Topčijs <kirilltopchy@gmail.com>
2021-11-28 17:42:04 +0200

    Exercise 5 completed.

---
+++ csharp-basics/exercises/Loops/Loops/Exercise 5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first word:");
+            var firstWord = Console.ReadLine();
+
+            Console.WriteLine("Enter second word:");
+            var secondWord = Console.ReadLine();
+
+            var result = firstWord;
+
+            for (int i = firstWord.Length; i < 30-secondWord.Length; i++)
+            {
+                result += ".";
+            }
+
+            result += secondWord;
+            Console.WriteLine(result);
+        }
+    }
+}
