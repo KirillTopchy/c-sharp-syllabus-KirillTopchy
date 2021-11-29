30340f94eb7fb460cf599c8b79bdd59047947a09
Kirils Topčijs <kirilltopchy@gmail.com>
2021-11-29 22:03:29 +0200

    Exercise 3 completed.

---
+++ csharp-basics/exercises/Loops/Loops/Exercise 3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array =  GenerateRandomNumbersArray();
+            Print(array);
+            Console.Write("Chose number: ");
+            int.TryParse(Console.ReadLine(), out var chosenNumber);
+            Console.WriteLine("Your numbers position in array is {0}",Array.IndexOf(array,chosenNumber));
+        }
+
+        static int[] GenerateRandomNumbersArray()
+        {
+            int[] randomNumbersArray = new int[20];
+
+            var random = new Random();
+
+            for (int i = 0; i < randomNumbersArray.Length; i++)
+            {
+                randomNumbersArray[i] = random.Next(1, 101);
+            }
+
+            return randomNumbersArray;
+        }
+
+        static void Print(int[] numbers)
+        {
+            foreach (var number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
