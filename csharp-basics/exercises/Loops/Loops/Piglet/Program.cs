32cfeebbacf446cbdec071de663665280f186846
Kirils Topčijs <kirilltopchy@gmail.com>
2021-11-30 22:47:15 +0200

    Exercise 7 completed.

---
+++ csharp-basics/exercises/Loops/Loops/Piglet/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Piglet
+{
+    class Program
+    {
+        private static int _totalScore = 0;
+        private static bool _gameLost = false;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Piglet!");
+            RollDie();
+            while (_gameLost == false && ContinueGame()) 
+            {
+                RollDie();
+            }
+            Console.WriteLine("You got {0} points.", _totalScore);
+        }
+
+        public static void RollDie()
+        {
+            Random random = new Random();
+            var randomDie =  random.Next(1, 7);
+            if (randomDie == 1)
+            {
+                _totalScore = 0;
+                _gameLost = true;
+            }
+            else
+            {
+                _totalScore += randomDie;
+            }
+            Console.WriteLine("You rolled a {0}!, total score is {1}", randomDie, _totalScore);
+        }
+
+        public static bool ContinueGame()
+        {
+            Console.WriteLine("Roll again? Press y if you want to stop press n");
+            var userChoice = Console.ReadKey(true).KeyChar.ToString().ToLower();
+            return userChoice == "y";
+        }
+    }
+}
