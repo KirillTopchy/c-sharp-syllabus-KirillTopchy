c0a1386ff839845e53c53886f737a3dc8da8c8ab
Kirils Topčijs <kirilltopchy@gmail.com>
2021-12-02 21:32:12 +0200

    Exercise 10 not finished.

---
+++ csharp-basics/exercises/Loops/Loops/NumberSquare/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace NumberSquare
+{
+    class Program
+    {
+        private static int _min;
+        private static int _max;
+        private static int _localMin;
+
+        public static void Main(string[] args)
+        {
+            UserInput();
+            PrintNumbers();
+        }
+
+        public static void UserInput()
+        {
+            Console.Write("Enter min value: ");
+            int.TryParse(Console.ReadLine(), out _min);
+            Console.Write("Enter max value: ");
+            int.TryParse(Console.ReadLine(), out _max);
+            _localMin += _min;
+        }
+
+        public static void PrintNumbers()
+        {
+            var result = "";
+            for (int i = _min; i <= _max; i++)
+            {
+                for (int j = _min; j <= _max; j++)
+                {
+                    result += j;
+                }
+                Console.Write(result);
+                var min = _localMin;
+                while (result.Length < _max)
+                {
+                    result += min;
+                    Console.Write(min);
+                    min++;
+                }
+                result = "";
+                _min ++;
+                Console.WriteLine();
+            }
+        }
+    }
+}
