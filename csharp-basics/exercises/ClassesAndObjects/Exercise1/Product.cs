192b9d0a023bbe129c49adf3dbb04ef2446061b6
Kirils Topčijs <kirilltopchy@gmail.com>
2021-12-04 07:56:50 +0200

    Exercise 1 complete.

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise1/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Product
+    {
+        private double _price { get; set; }
+        private int _amount { get; set; }
+        private readonly string _name;
+
+        public Product(string name, double priceAtStart, int amountAtStart)
+        {
+            _name = name;
+            _amount = amountAtStart;
+            _price = priceAtStart;
+        }
+
+        public void PrintProduct()
+        {
+            Console.WriteLine("{0}, price {1}, amount {2}", _name, _price, _amount);
+        }
+
+        public int Amount
+        {
+            set => _amount = value;
+        }
+
+        public double Price
+        {
+            set => _price = value;
+        }
+
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mouse = new Product("Logitech mouse", 70.00, 14);
+            var iPhone = new Product("iPhone 5s", 999.99, 3);
+            var projector = new Product("Epson EB-U05", 440.46, 1);
+
+            Console.WriteLine("Initial values.");
+            mouse.PrintProduct();
+            iPhone.PrintProduct();
+            projector.PrintProduct();
+
+            Console.WriteLine("Changed values.");
+            mouse.Amount = 13;
+            mouse.Price = 69.99;
+            mouse.PrintProduct();
+
+            iPhone.Amount = 4;
+            iPhone.Price = 899.99;
+            iPhone.PrintProduct();
+
+            projector.Amount = 0;
+            projector.Price = 599.99;
+            projector.PrintProduct();
+        }
+    }
+}
