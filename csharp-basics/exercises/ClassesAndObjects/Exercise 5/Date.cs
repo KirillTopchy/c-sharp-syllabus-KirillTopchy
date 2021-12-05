4350bbf586adbecf3e2478a087111dce535e8b38
Kirils Topčijs <kirilltopchy@gmail.com>
2021-12-05 16:37:40 +0200

    Exercise 5 complete.

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 5/Date.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_5
+{
+    class Date
+    {
+        private int Day { get; set; }
+        private int Month { get; set; }
+        private int Year { get; set; }
+
+        public Date(int day, int month, int year)
+        {
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+
+        public void DisplayDate()
+        {
+            Console.WriteLine($"{Month}/{Day}/{Year}");
+        }   
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 5/DateTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exercise_5
+{
+    class DateTest
+    {
+        static void Main(string[] args)
+        {
+            var date1 = new Date(8, 3, 1994);
+            date1.DisplayDate();
+        }
+    }
+}
