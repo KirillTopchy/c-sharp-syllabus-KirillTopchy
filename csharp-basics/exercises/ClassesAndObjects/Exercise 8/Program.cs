1b3ff402d8c114734f06a82dc77267089348699d
Kirils Topčijs <kirilltopchy@gmail.com>
2021-12-05 21:13:11 +0200

    Exercise 8 completed.

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 8/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Exercise_8
+{
+    class Program
+    {
+        private static double _interestRate = 0.0;
+        private static int _depositPeriodInMonths = 0;
+        private static double _startBalance = 0;
+
+        static void Main(string[] args)
+        {
+            InitialDepositInformation();
+            var userStartBalance = new SavingsAccount(_startBalance);
+            DepositsAndWithdrawalsDuringDepositPeriod(userStartBalance);
+            AccountInfoAtTheEndOfDepositPeriod(userStartBalance);
+        }
+
+        public static void InitialDepositInformation()
+        {
+            Console.Write("How much money is in the account?: ");
+            double.TryParse(Console.ReadLine(), out _startBalance);
+            Console.Write("Enter the annual interest rate: ");
+            double.TryParse(Console.ReadLine(), out _interestRate);
+            Console.Write("How long has the account been opened?: ");
+            int.TryParse(Console.ReadLine(), out _depositPeriodInMonths);
+        }
+
+        public static void DepositsAndWithdrawalsDuringDepositPeriod(SavingsAccount account)
+        {
+            for (int i = 0; i < _depositPeriodInMonths ; i++)
+            {
+                Console.Write("Enter amount deposited for month: {0}: ", i + 1);
+                double.TryParse(Console.ReadLine(), out var deposit);
+                account.Deposit(deposit);
+                Console.Write("Enter amount withdrawn for month: {0}: ", i + 1);
+                double.TryParse(Console.ReadLine(), out var withdraw);
+                account.Withdrawal(withdraw);
+                account.MonthlyInterest(_interestRate);
+            }
+        }
+
+        public static void AccountInfoAtTheEndOfDepositPeriod(SavingsAccount account)
+        {
+            Console.WriteLine("Total deposited: {0:C2}", Convert.ToDecimal(account.GetDeposits()));
+            Console.WriteLine("Total withdrawn: {0:C2}", Convert.ToDecimal(account.GetWithdrawals()));
+            Console.WriteLine("Interest earned: {0:C2}", Convert.ToDecimal(account.MonthlyInterest(_interestRate)));
+            Console.WriteLine("Ending balance: {0:C2}", Convert.ToDecimal(account.GetBalance()));
+        }
+
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 8/SavingsAccount.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_8
+{
+    class SavingsAccount
+    {
+        private double AnnualInterestRate { get; set; }
+        private double Balance { get; set; }
+
+        private double Deposits { get; set; }
+
+        private double Withdrawals { get; set; }
+
+        private double InterestEarned { get; set; }
+
+        public SavingsAccount(double startBalance)
+        {
+            Balance = startBalance;
+        }
+
+        public double GetBalance()
+        {
+            Balance += InterestEarned;
+            return Balance;
+        }
+
+        public double GetDeposits()
+        {
+            return Deposits;
+        }
+
+        public double GetWithdrawals()
+        {
+            return Withdrawals;
+        }
+
+        public double Withdrawal(double withdrawalAmount)
+        {
+            Withdrawals += withdrawalAmount;
+            return Balance -= withdrawalAmount;
+        }
+
+        public double Deposit(double depositAmount)
+        {
+            Deposits += depositAmount;
+            return Balance += depositAmount;
+        }
+
+        public double MonthlyInterest(double interestRate)
+        {
+            AnnualInterestRate = interestRate;
+            return InterestEarned = Balance * AnnualInterestRate/12*4/100;
+        }
+    }
+}
