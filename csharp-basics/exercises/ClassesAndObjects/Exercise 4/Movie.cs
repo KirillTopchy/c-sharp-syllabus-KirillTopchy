290b20a86744eb5c5fc053683f5fd3b173743323
Kirils Topčijs <kirilltopchy@gmail.com>
2021-12-05 16:25:21 +0200

    Exercise 4 complete.

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 4/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Exercise_4
+{
+    class Movie
+    {
+        private string _title { get; set; }
+        private string _studio { get; set; }
+        public string Rating { get; private set; }
+
+        public Movie(string title, string studio, string rating)
+        {
+            _title = title;
+            _studio = studio;
+            Rating = rating;
+        }
+
+        public Movie(string title, string studio)
+        {
+            _title = title;
+            _studio = studio;
+            Rating = "PG";
+        }
+
+        public string Display()
+        {
+            return $"{_title} {_studio} {Rating}";
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstMovie = new Movie("Casino Royale", "Eon Productions", "PG13");
+            var secondMovie = new Movie("Glass", "Buena Vista International", "PG13");
+            var thirdMovie = new Movie("Spider-Man: Into the Spider-Verse", "Columbia Pictures", "PG");
+
+            var movies = new List<Movie>()
+            {
+                firstMovie,
+                secondMovie,
+                thirdMovie
+            };
+            Console.WriteLine("All movies");
+            PrintInfo(movies);
+
+            Console.WriteLine("Movies with PG rating");
+            var filtered = GetPg(movies.ToArray());
+            PrintInfo(filtered);
+        }
+
+        public static Movie[] GetPg(Movie[] movies)
+        {
+            return movies.Where(movie => movie.Rating == "PG").ToArray();
+        }
+
+        public static void PrintInfo(Movie[] movies)
+        {
+            foreach (var movie in movies)
+            {
+                Console.WriteLine(movie.Display());
+            }
+        }
+
+        public static void PrintInfo(List<Movie> movies)
+        {
+            foreach (var movie in movies)
+            {
+                Console.WriteLine(movie.Display());
+            }
+            Console.WriteLine();
+        }
+    }
+}
