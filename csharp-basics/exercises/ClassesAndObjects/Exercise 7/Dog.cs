0ff4407eb60edd730296764418ef62f5dd9b1736
Kirils Topčijs <kirilltopchy@gmail.com>
2021-12-05 19:33:21 +0200

    Exercise 7 complete.

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 7/Dog.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_7
+{
+    class Dog
+    {
+        private string Name { get; set; }
+        private string Sex { get; set; }
+        private string Mother { get; set; }
+        private string Father { get; set; }
+
+        public Dog(string name, string sex)
+        {
+            Name = name;
+            Sex = sex;
+        }
+
+        public Dog(string name, string sex, string mother, string father)
+        {
+            Name = name;
+            Sex = sex;
+            Mother = mother;
+            Father = father;
+        }
+
+        public string FathersName()
+        {
+            return Father ?? "Unknown";
+        }
+
+        public bool HasSameMotherAs(Dog dog1)
+        {
+            return dog1.Mother == Mother;
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 7/DogTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise_7
+{
+    class DogTest
+    {
+        static void Main(string[] args)
+        {
+            var max = new Dog("Max", "male","Lady", "Rocky");
+            var rocky = new Dog("Rocky", "male", "Molly", "Sam");
+            var sparky = new Dog("Sparky", "male");
+            var buster = new Dog("Buster", "male", "Lady", "Sparky");
+            var sam = new Dog("Sam", "male");
+            var lady = new Dog("Lady", "female");
+            var molly = new Dog("Molly", "female");
+            var coco = new Dog("Coco", "female", "Molly", "Buster");
+
+            Console.WriteLine(coco.FathersName());
+            Console.WriteLine(sparky.FathersName());
+            Console.WriteLine(coco.HasSameMotherAs(rocky));
+            Console.WriteLine(coco.HasSameMotherAs(buster));
+            Console.WriteLine(coco.HasSameMotherAs(sam));
+        }
+    }
+}
