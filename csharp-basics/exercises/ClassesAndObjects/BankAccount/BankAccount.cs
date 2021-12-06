6423a36535eed4159fe9387bf5a5098f715fdae8
Kirils Topčijs <kirilltopchy@gmail.com>
2021-12-06 21:45:20 +0200

    Exercise 10 complete.

---
+++ csharp-basics/exercises/ClassesAndObjects/BankAccount/BankAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    class BankAccount
+    {
+        private string Name { get; set; }
+        private  double Balance { get; set; }
+
+        public BankAccount(string name, double balance)
+        {
+            Name = name;
+            Balance = balance;
+        }
+
+        public string ShowUserNameAndBalance()
+        {
+            if (Balance < 0)
+            {
+                const string fmt1 = "#,##0.00";
+                var output = Name.ToString() + ", -$" + Math.Abs(Balance).ToString(fmt1);
+                return output;
+            }
+            return $"{Name}, {Balance:C2}";
+        }
+
+    }
+}
--- csharp-basics/exercises/ClassesAndObjects/BankAccount/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/BankAccount/Program.cs
@@ -10,6 +10,11 @@ namespace BankAccount
     {
         static void Main(string[] args)
         {
+            var user1 = new BankAccount("Benson", -17.5);
+            var user2 = new BankAccount("Tom", 17.5);
+            Console.WriteLine(user1.ShowUserNameAndBalance());
+            Console.WriteLine(user2.ShowUserNameAndBalance());
+            Console.ReadKey();
         }
     }
 }
