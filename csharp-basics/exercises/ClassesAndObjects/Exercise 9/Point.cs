90709ab28660bfc5a21ab77f6ffd198c296fb2cc
Kirils Topčijs <kirilltopchy@gmail.com>
2021-12-06 21:08:37 +0200

    Exercise 9 complete.

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 9/Point.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_9
+{
+    class Point
+    {
+        private int X { get; set; }
+        private int Y { get; set; }
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void SwapPoints(Point p1, Point p2)
+        {
+            var xHolder = p1.X;
+            var yHolder = p1.Y;
+            p1.Y = p2.Y;
+            p1.X = p2.X;
+            p2.Y = yHolder;
+            p2.X = xHolder;
+        }
+
+        public int GetX()
+        {
+            return X;
+        }
+
+        public int GetY()
+        {
+            return Y;
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 9/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercise_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Point(5, 2);
+            var p2 = new Point(-3, 6);
+            p1.SwapPoints(p1,p2);
+            Console.WriteLine("(" + p1.GetX() + ", " + p1.GetY() + ")");
+            Console.WriteLine("(" + p2.GetX() + ", " + p2.GetY() + ")");
+        }
+    }
+}
