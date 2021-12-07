914803de22d93c1303eb074333c6c5bef02358b1
Kirils Topčijs <kirilltopchy@gmail.com>
2021-12-07 19:11:13 +0200

    Exercise 6 updated.

--- csharp-basics/exercises/Loops/Loops/FizzBuzz/FizzBuzz.cs
+++ csharp-basics/exercises/Loops/Loops/FizzBuzz/FizzBuzz.cs
@@ -11,37 +11,26 @@ namespace Exercise_6
 
             for (int i = 1; i <= enteredInteger; i++)
             {
-                if (i % 3 == 0 && i % 5 == 0 && i % 20 == 0)
-                {
-                    Console.WriteLine("FizzBuzz ");
-                }
-                else if (i % 3 == 0 && i % 5 == 0)
+                if (i % 3 == 0 && i % 5 == 0)
                 {
                     Console.Write("FizzBuzz ");
                 }
-                else if (i % 3 == 0 && i % 20 == 0)
-                {
-                    Console.WriteLine("Fizz ");
-                }
-                else if (i % 3 == 0)
+                else if (i % 3 == 0) 
                 {
                     Console.Write("Fizz ");
-                }
-                else if (i % 5 == 0 && i % 20 == 0)
-                {
-                    Console.WriteLine("Buzz ");
                 }
                 else if (i % 5 == 0)
                 {
                     Console.Write("Buzz ");
                 }
-                else if(i % 20 == 0)
-                {
-                    Console.WriteLine(i + " ");
-                }
                 else
                 {
                     Console.Write(i + " ");
+                }
+
+                if (i % 20 == 0)
+                {
+                    Console.WriteLine();
                 }
             }
         }
