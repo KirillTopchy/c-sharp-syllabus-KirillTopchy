825abfdf913debaa2e9c3f6e27a00ed09628a76b
Kirils Topčijs <kirilltopchy@gmail.com>
2021-12-09 20:48:14 +0200

    Exercise 2.5 complete.

--- csharp-basics/exercises/Collections/ListExercise5/Program.cs
+++ csharp-basics/exercises/Collections/ListExercise5/Program.cs
@@ -22,8 +22,10 @@ namespace ListExercise5
             Console.WriteLine(string.Join(",", colors));
 
             //TODO: Change the third element with "Yellow"
+            colors[2] = "Yellow";
 
             Console.WriteLine(string.Join(",", colors));
+            Console.ReadKey();
         }
     }
 }
