f8945d1f8468169c2ea18317c4d078e292ec0683
Kirils Topčijs <kirilltopchy@gmail.com>
2021-12-09 21:18:49 +0200

    Exercise 2.11 complete.

--- csharp-basics/exercises/Collections/ListExercise11/Program.cs
+++ csharp-basics/exercises/Collections/ListExercise11/Program.cs
@@ -11,18 +11,41 @@ namespace ListExercise11
         static void Main(string[] args)
         {
             //TODO: Create an List with string elements
-
             //TODO: Add 10 values to list
+            var stringList = new List<string> 
+            {
+                "a",
+                "b",
+                "c",
+                "d",
+                "e",
+                "f",
+                "g",
+                "h",
+                "i",
+                "j"
+            };
 
             //TODO: Add new value at 5th position
+            stringList.Insert(4,"aa");
 
             //TODO: Change value at last position (Calculate last position programmatically)
+            stringList[stringList.Count - 1] = "TEST";
 
             //TODO: Sort your list in alphabetical order
+            stringList.Sort();
 
             //TODO: Check if your list contains "Foobar" element
+            var contains = stringList.Contains("Foobar");
+            Console.WriteLine(contains);
 
             //TODO: Print each element of list using loop
+            foreach (var s in stringList)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.ReadKey();
         }
     }
 }
