e4832c9b533b0d1c64e0eff014a93ce760b0da15
Kirils Topčijs <kirilltopchy@gmail.com>
2021-12-09 20:31:38 +0200

    Exercise 1 complete.

--- csharp-basics/exercises/Collections/Exercise1/Program.cs
+++ csharp-basics/exercises/Collections/Exercise1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -24,9 +25,50 @@ namespace Exercise1
 
             //todo - replace array with an List and print out the results
 
+            var carBrands = new List<string>();
+            carBrands.AddRange(array);
+            foreach (var car in carBrands)
+            {
+                Console.WriteLine(car);
+            }
+            Console.WriteLine();
+
             //todo - replace array with a HashSet and print out the results
 
+            var carsHashSet = new HashSet<string>
+            {
+                "Audi",
+                "BMW",
+                "Honda",
+                "Mercedes",
+                "VolksWagen",
+                "Mercedes",
+                "Tesla"
+            };
+
+            foreach (var car in carsHashSet)
+            {
+                Console.WriteLine(car);
+            }
+            Console.WriteLine();
+
             //todo - replace array with a Dictionary (use brand as key and origination as value) and print out the results
+
+            var carsDictionary = new Dictionary<string, string>
+            {
+                {"Audi", "Germany"},
+                {"BMW", "Germany"},
+                {"Honda", "Japan"},
+                {"Mercedes", "Germany"},
+                {"VolksWagen", "Germany"},
+                {"Tesla", "USA"}
+            };
+
+            foreach (var car in carsDictionary)
+            {
+                Console.WriteLine(car.Key+ " " +car.Value);
+            }
+            Console.ReadKey();
         }
     }
 }
