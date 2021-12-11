9c4ff1afccec3d48df7ac18b243f5ce4db28a7eb
Kirils Topčijs <kirilltopchy@gmail.com>
2021-12-11 10:45:28 +0200

    Exercise 9 complete.

---
+++ csharp-basics/exercises/Collections/Exercise 9/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hashSet = new HashSet<string>
+            {
+                "1",
+                "2",
+                "3",
+                "4",
+                "5"
+            };
+
+            foreach (var s in hashSet)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine();
+            hashSet.Clear();
+            Console.WriteLine(hashSet.Count);
+
+            hashSet.Add("1");
+            hashSet.Add("1");
+
+            foreach (var s in hashSet)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(hashSet.Count);
+            Console.ReadKey();
+        }
+    }
+}
