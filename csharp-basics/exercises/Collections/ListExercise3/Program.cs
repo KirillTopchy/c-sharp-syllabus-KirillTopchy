09643cfbc2bc4dc42ce4c0075bbe0e4b2e168fcd
Kirils Topčijs <kirilltopchy@gmail.com>
2021-12-13 08:25:55 +0200

    Exercise 2.3 updated.

--- csharp-basics/exercises/Collections/ListExercise3/Program.cs
+++ csharp-basics/exercises/Collections/ListExercise3/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ListExercise3
 {
@@ -10,8 +7,6 @@ namespace ListExercise3
     {
         static void Main(string[] args)
         {
-            //TODO: Write a C# program to insert an element into the array list at the first position.
-
             var colors = new List<string>
             {
                 "Red",
@@ -24,8 +19,6 @@ namespace ListExercise3
             colors.Insert(0,"Blue");
 
             Console.WriteLine(string.Join(",", colors));
-
-            //TODO: Add new color at the first and third position of the list
 
             colors.Insert(0,"Pink");
             colors.Insert(2, "Brown");
