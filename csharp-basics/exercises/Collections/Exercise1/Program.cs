a70f63f4a2e65cee0a15283d0b4ace19c921142f
Kirils Topčijs <kirilltopchy@gmail.com>
2021-12-13 08:22:51 +0200

    Exercise1 updated.

--- csharp-basics/exercises/Collections/Exercise1/Program.cs
+++ csharp-basics/exercises/Collections/Exercise1/Program.cs
@@ -1,29 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Security.AccessControl;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Exercise1
 {
     class Program
     {
-        /**
-           * Origination:
-           * Audi -> Germany
-           * BMW -> Germany
-           * Honda -> Japan
-           * Mercedes -> Germany
-           * VolksWagen -> Germany
-           * Tesla -> USA
-           */
-
         private static void Main(string[] args)
         {
             string[] array = { "Audi", "BMW", "Honda", "Mercedes", "VolksWagen", "Mercedes", "Tesla" };
-
-            //todo - replace array with an List and print out the results
 
             var carBrands = new List<string>();
             carBrands.AddRange(array);
@@ -32,8 +16,6 @@ namespace Exercise1
                 Console.WriteLine(car);
             }
             Console.WriteLine();
-
-            //todo - replace array with a HashSet and print out the results
 
             var carsHashSet = new HashSet<string>
             {
@@ -51,8 +33,6 @@ namespace Exercise1
                 Console.WriteLine(car);
             }
             Console.WriteLine();
-
-            //todo - replace array with a Dictionary (use brand as key and origination as value) and print out the results
 
             var carsDictionary = new Dictionary<string, string>
             {
