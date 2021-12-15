78db4e3ee09835480c65fc90fa2e6a61be02b932
Kirils Topčijs <kirilltopchy@gmail.com>
2021-12-15 17:19:34 +0200

    Exercise 3 complete.

---
+++ csharp-basics/exercises/Polymorphism/Exercise 3/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise_3
+{
+    class Employee : Person
+    {
+        private string _jobTitle;
+
+        public Employee(string jobTitle, string firstName, string lastName, string address, int id) : base(firstName, lastName, address, id)
+        {
+            _jobTitle = jobTitle;
+        }
+
+        public string JobTitle
+        {
+            get => _jobTitle;
+            set => _jobTitle = value;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine($"First name: {FirstName} \nLast name: {LastName} \nAddress: {Address} \nID: {Id} \nJob title: {JobTitle}");
+        }
+    }
+}
---
+++ csharp-basics/exercises/Polymorphism/Exercise 3/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise_3
+{
+    class Student : Person
+    {
+        private double _gpa;
+
+        public Student(double gpa, string firstName, string lastName, string address, int id) : base(firstName, lastName, address, id)
+        {
+            _gpa = gpa;
+        }
+
+        public double Gpa
+        {
+            get => _gpa;
+            set => _gpa = value;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine($"First name: {FirstName} \nLast name: {LastName} \nAddress: {Address} \nID: {Id} \nGPA: {Gpa}");
+        }
+    }
+}
