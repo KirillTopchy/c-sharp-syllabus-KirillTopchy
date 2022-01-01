b01fae25221f4ef38f5edc1c3ceda6266f870549
Kirils Topčijs <kirilltopchy@gmail.com>
2022-01-01 11:18:24 +0200

    Array Exercise 6 Tests not finished.

---
+++ csharp-basics/exercises/Arrays/Arrays/Exercise 6.Tests/CreateArrayTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercise_6;
+
+namespace Exercise_6.Tests
+{
+    [TestClass]
+    public class CreateArrayTests
+    {
+        [TestMethod]
+        public void CreateRandomNumberArray_10_ShouldReturnRandomNumberArrayWithLength10()
+        {
+            //Arrange
+
+            //Act
+            
+            //Assert
+        }
+    }
+}
---
+++ csharp-basics/exercises/Arrays/Arrays/Exercise 6/CreateArray.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exercise_6
+{
+    public class CreateArray
+    {
+        private static readonly int _min = 1;
+        private static readonly int _max = 101;
+
+        public static int[] CreateRandomNumberArray(int length)
+        {
+            int[] firstRandomNumbersArray = new int[length];
+
+            for (int i = 0; i < firstRandomNumbersArray.Length; i++)
+            {
+                var random = new Random();
+                firstRandomNumbersArray[i] = random.Next(_min, _max);
+            }
+
+            return firstRandomNumbersArray;
+        }
+
+        public static int [] CopyArray(int[] firstArray)
+        {
+            var secondRandomNumbersArray = new int[firstArray.Length];
+            Array.Copy(firstArray, secondRandomNumbersArray, firstArray.Length);
+            return secondRandomNumbersArray;
+        }
+
+        public static void ReplacLastElementOfArrayWith7(int[] firstArray)
+        {
+            firstArray[^1] = -7;
+        }
+
+        public static void PrintArray(int[] array)
+        {
+            foreach (var element in array)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
