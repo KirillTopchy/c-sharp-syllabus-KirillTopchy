3bf68df3f6679594f0489f101438247b791f981a
Kirils Topčijs <kirilltopchy@gmail.com>
2022-01-02 12:02:03 +0200

    Exercise_8 Arithmetics test completed.

--- csharp-basics/exercises/Arithmetic/Exercise 8/Program.cs
+++ csharp-basics/exercises/Arithmetic/Exercise 8/Program.cs
@@ -6,48 +6,14 @@ namespace Exercise_8
     {
         static void Main(string[] args)
         {
-            SalaryCalculator((decimal) 7.5, 35);
-            SalaryCalculator((decimal)8.20, 47);
-            SalaryCalculator((decimal)10.00, 73);
-        }
+            var salary1 = SalaryCalculator.CalculateSalary(7.5m, 35);
+            Console.WriteLine(SalaryCalculator.DisplayMessage(salary1, 7.5m, 35));
 
-        public static void SalaryCalculator(decimal basePay, double hoursWorked)
-        {
-            decimal salary = 0;
-            double overtime = 0;
-            var employee = new Employee
-            {
-                BasePay = basePay,
-                HoursWorked = hoursWorked
-            };
+            var salary2 = SalaryCalculator.CalculateSalary(8.20m, 47);
+            Console.WriteLine(SalaryCalculator.DisplayMessage(salary2, 8.20m, 47));
 
-            if (employee.BasePay < (decimal) 8.00)
-            {
-                employee.BasePay = (decimal)8.00;
-            }
-
-            if (employee.HoursWorked > 40)
-            {
-                overtime = employee.HoursWorked - 40;
-            }
-
-            switch (employee.HoursWorked)
-            {
-                case > 60:
-                    Console.WriteLine("Foo Corp requires that an employee does not work more than 60 hours in a week.");
-                    break;
-
-                case > 40 and < 60:
-                    salary = (decimal) (((employee.HoursWorked - overtime) * (double) employee.BasePay) +
-                                        (overtime * (double) employee.BasePay * 1.5));
-                    Console.WriteLine("Employee base pay: {0}, hours worked:{1}, salary: {2}$",employee.BasePay, employee.HoursWorked,salary);
-                    break;
-
-                default:
-                    salary = (decimal) (employee.HoursWorked * (double) employee.BasePay);
-                    Console.WriteLine("Employee base pay: {0}, hours worked:{1}, salary: {2}$", employee.BasePay, employee.HoursWorked, salary);
-                    break;
-            }
+            var salary3 = SalaryCalculator.CalculateSalary(10.00m, 73);
+            Console.WriteLine(SalaryCalculator.DisplayMessage(salary3, 10.00m, 73));
         }
     }
 }
