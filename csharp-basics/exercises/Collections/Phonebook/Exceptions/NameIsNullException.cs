77333c6a1d0f7096751574749ac02895407acc97
Kirils Topčijs <kirilltopchy@gmail.com>
2022-01-02 16:42:05 +0200

    Exercise_8 Collections test completed.

---
+++ csharp-basics/exercises/Collections/Phonebook/Exceptions/NameIsNullException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook.Exceptions
+{
+    public class NameIsNullException : Exception
+    {
+        public NameIsNullException()
+        {
+        }
+
+        public NameIsNullException(string message) : base(message)
+        {
+        }
+
+        public NameIsNullException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
---
+++ csharp-basics/exercises/Collections/Phonebook/Exceptions/NumberIsNullException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook
+{
+    public class NumberIsNullException : Exception
+    {
+        public NumberIsNullException()
+        {
+        }
+
+        public NumberIsNullException(string message) : base(message)
+        {
+        }
+
+        public NumberIsNullException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
