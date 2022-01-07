1c8056ee969d92c25f87c0e904090ca20d798a2a
Kirils Topčijs <kirilltopchy@gmail.com>
2022-01-07 21:38:29 +0200

    MatchingGame finished.

--- csharp-basics/exercises/MiniProjects/MatchingGame/MatchingGame/Form1.Designer.cs
+++ csharp-basics/exercises/MiniProjects/MatchingGame/MatchingGame/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace MatchingGame
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.label13 = new System.Windows.Forms.Label();
             this.label14 = new System.Windows.Forms.Label();
@@ -46,6 +47,8 @@ namespace MatchingGame
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -277,6 +280,16 @@ namespace MatchingGame
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label1.Click += new System.EventHandler(this.label_Click);
             // 
+            // timer1
+            // 
+            this.timer1.Interval = 750;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
@@ -309,6 +322,8 @@ namespace MatchingGame
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
     }
 }
 
--- csharp-basics/exercises/MiniProjects/MatchingGame/MatchingGame/Form1.cs
+++ csharp-basics/exercises/MiniProjects/MatchingGame/MatchingGame/Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MatchingGame
@@ -22,16 +17,18 @@ namespace MatchingGame
         private Label _secondClicked = null;
 
         // Use this Random object to choose random icons for the squares
-        Random random = new();
+        private readonly Random _random = new();
 
         // Each of these letters is an interesting icon
         // in the Webdings font,
         // and each icon appears twice in this list
-        List<string> icons = new()
+        private readonly List<string> _icons = new()
         {
             "!", "!", "N", "N", ",", ",", "k", "k",
             "b", "b", "v", "v", "w", "w", "z", "z"
         };
+
+        private double _totalTime;
 
         public Form1()
         {
@@ -49,10 +46,10 @@ namespace MatchingGame
             {
                 if (control is Label iconLabel)
                 {
-                    var randomNumber = random.Next(icons.Count);
-                    iconLabel.Text = icons[randomNumber];
+                    var randomNumber = _random.Next(_icons.Count);
+                    iconLabel.Text = _icons[randomNumber];
                     iconLabel.ForeColor = iconLabel.BackColor;
-                    icons.RemoveAt(randomNumber);
+                    _icons.RemoveAt(randomNumber);
                 }
             }
         }
@@ -66,6 +63,14 @@ namespace MatchingGame
         {
             if (sender is Label clickedLabel)
             {
+                // The timer is only on after two non-matching 
+                // icons have been shown to the player, 
+                // so ignore any clicks if the timer is running
+                if (timer1.Enabled)
+                {
+                    return;
+                }
+
                 // If the clicked label is black, the player clicked
                 // an icon that's already been revealed --
                 // ignore the click
@@ -83,12 +88,88 @@ namespace MatchingGame
                     _firstClicked = clickedLabel;
                     _firstClicked.ForeColor = Color.Black;
                 }
-                else if(_secondClicked == null)
+
+                // If the player gets this far, the timer isn't
+                // running and firstClicked isn't null,
+                // so this must be the second icon the player clicked
+                // Set its color to black
+                else if (_secondClicked == null)
                 {
                     _secondClicked = clickedLabel;
                     _secondClicked.ForeColor = Color.Black;
+
+                    // If the player gets this far, the player 
+
+                    // Check to see if the player won
+                    if (CheckWinner())
+                    {
+                        // If the loop didn’t return, it didn't find
+                        // any unmatched icons
+                        // That means the user won. Show a message and close the form
+                        timer2.Stop();
+                        MessageBox.Show(@$"You WIN! Tour time: " + Math.Round(_totalTime, 2) + @" seconds.", @"Congratulations");
+                        Close();
+                    }
+
+                    // clicked two different icons, so start the 
+                    // timer (which will wait three quarters of 
+                    // a second, and then hide the icons)
+                    timer1.Start();
                 }
             }
+        }
+
+        /// <summary>
+        /// This timer is started when the player clicks 
+        /// two icons that don't match,
+        /// so it counts three quarters of a second 
+        /// and then turns itself off and hides both icons
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Stop the timer
+            timer1.Stop();
+
+            if (_firstClicked.Text != _secondClicked.Text)
+            {
+                // Hide both icons
+                _firstClicked.ForeColor = _firstClicked.BackColor;
+                _secondClicked.ForeColor = _secondClicked.BackColor;
+            }
+
+            // Reset firstClicked and secondClicked 
+            // so the next time a label is
+            // clicked, the program knows it's the first click
+            _firstClicked = null;
+            _secondClicked = null;
+        }
+
+        /// <summary>
+        /// Check every icon to see if it is matched, by 
+        /// comparing its foreground color to its background color. 
+        /// If all of the icons are matched, the player wins
+        /// </summary>
+        private bool CheckWinner()
+        {
+            // Go through all of the labels in the TableLayoutPanel, 
+            // checking each one to see if its icon is matched
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                if (control is Label iconLabel)
+                {
+                    if (iconLabel.ForeColor == iconLabel.BackColor)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            _totalTime += 0.1;
         }
     }
 }
