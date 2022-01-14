ed148ac6a5806739636bb11c74816e72b4c634a8
Kirils Topčijs <kirilltopchy@gmail.com>
2022-01-14 20:16:04 +0200

    Sudoku grid created, all buttons are click able.

--- GameJam2022/Sudoku/Sudoku/Form1.cs
+++ GameJam2022/Sudoku/Sudoku/Form1.cs
@@ -12,10 +12,69 @@ namespace Sudoku
 {
     public partial class Form1 : Form
     {
+        public int count = 1;
+
+        public bool isClicked;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+
+            if (isClicked == false)
+            {
+
+                if (count != 10)
+                {
+                    button.Text = count.ToString();
+                    count++;
+                    return;
+                }
+
+                if (count == 10)
+                {
+                    count = 1;
+                }
+            }
+            else
+            {
+                var localCounter = int.Parse(button.Text);
+                if (button.Text != "10")
+                {
+                    button.Text = localCounter.ToString();
+                    localCounter++;
+                }
+
+                if (localCounter == 10)
+                {
+                    localCounter = 1;
+                }
+            }
+
+            if(count != 10)
+            {
+                button.Text = count.ToString();
+                count++;
+            }
+
+            if (count == 10)
+            {
+                count = 1;
+            }
+        }
+
+        public bool isClicke()
+        {
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
